6d6484fc6f91af21be8b508183f8300bb4ecbeb6
Shawn Jiang <shawn.x.jiang@gmail.com>
2024-05-12 22:07:09 -0500

    finish the basic logic of the rigid simulator without collision processing and validate the stuff

--- demo_rigid_taichi.py
+++ demo_rigid_taichi.py
@@ -3,6 +3,30 @@ import numpy as np
 import taichi as ti
 
 
+@ti.func
+def SetToRotate(q):
+    qw, qx, qy, qz = q[3], q[0], q[1], q[2]
+    return ti.Matrix(
+        [
+            [
+                1 - 2 * (qy * qy + qz * qz),
+                2 * (qx * qy + qz * qw),
+                2 * (qx * qz - qy * qw),
+            ],
+            [
+                2 * (qx * qy - qz * qw),
+                1 - 2 * (qx * qx + qz * qz),
+                2 * (qy * qz + qx * qw),
+            ],
+            [
+                2 * (qx * qz + qy * qw),
+                2 * (qy * qz - qx * qw),
+                1 - 2 * (qx * qx + qy * qy),
+            ],
+        ]
+    ).transpose()
+
+
 @ti.data_oriented
 class RigidObjectSimulator:
     def __init__(
@@ -14,6 +38,7 @@ class RigidObjectSimulator:
         self.n_vertices = len(r)
         self.masses = ti.field(dtype=ti.f32, shape=self.n_vertices)
         self.masses.from_numpy(init_masses)
+        self.forces = ti.Vector.field(3, dtype=ti.f32, shape=self.n_vertices)
         # Rigid body model
         self.center = ti.Vector.field(3, dtype=ti.f32, shape=1)
         self.center.from_numpy(center.reshape(3, 1))
@@ -21,8 +46,11 @@ class RigidObjectSimulator:
         self.total_force = ti.Vector.field(3, dtype=ti.f32, shape=1)
         # Intialize the stuffs related to the rotation
         self.omega = ti.Vector.field(3, dtype=ti.f32, shape=1)
+        self.rot_q_inc = ti.Vector.field(4, dtype=ti.f32, shape=1)
+        self.rot_inc = ti.Matrix.field(3, 3, dtype=ti.f32, shape=1)
         self.total_torque = ti.Vector.field(3, dtype=ti.f32, shape=1)
         # Initialize the inverse mass and inverse inertia
+        # r and interia here is updated every step
         self.r = ti.Vector.field(3, dtype=ti.f32, shape=self.n_vertices)
         self.r.from_numpy(r)
         self.total_mass = ti.field(dtype=ti.f32, shape=1)
@@ -32,9 +60,6 @@ class RigidObjectSimulator:
         self.dt = dt
         self.num_substeps = num_substeps
         self.drag_damping = drag_damping
-        import pdb
-
-        pdb.set_trace()
 
     @ti.kernel
     def _init_rigid_attributes(self):
@@ -44,7 +69,7 @@ class RigidObjectSimulator:
 
         for idx_vertice in range(self.n_vertices):
             self.inertia[0] += self.masses[idx_vertice] * (
-                self.r[idx_vertice].norm_sqr() * ti.Matrix.identity(ti.f32, 3     )
+                self.r[idx_vertice].norm_sqr() * ti.Matrix.identity(ti.f32, 3)
                 - self.r[idx_vertice].outer_product(self.r[idx_vertice])
             )
 
@@ -58,13 +83,59 @@ class RigidObjectSimulator:
 
     @ti.func
     def clear_forces(self):
+        # Initialize the forces and torques
+        for idx_vertice in range(self.n_vertices):
+            self.forces[idx_vertice] = ti.Vector([0.0, 0.0, 0.0])
         self.total_force[0] = ti.Vector([0.0, 0.0, 0.0])
         self.total_torque[0] = ti.Vector([0.0, 0.0, 0.0])
 
     @ti.kernel
     def substep(self):
         self.clear_forces()
-        self.total_force[0] += ti.Vector([0.0, 0.0, -9.8]) * self.total_mass[0]
+        # Process the translation
+        for idx_vertice in range(self.n_vertices):
+            self.forces[idx_vertice] += (
+                ti.Vector([0.0, 0.0, -0.1]) * self.masses[idx_vertice]
+            )
+            self.total_force[0] += self.forces[idx_vertice]
+        self.v[0] += self.total_force[0] * self.dt / self.total_mass[0]
+        self.center[0] += self.v[0] * self.dt
+        # Process the rotation
+        for idx_vertice in range(self.n_vertices):
+            # Here is a option to add torque to make the case where center is not gradity center work; However, it will make the system complicated
+            # The normal things work as long as the center is the gravity center
+            # self.total_torque[0] += self.r[idx_vertice].cross(
+            #     self.forces[idx_vertice]
+            # ) - self.masses[idx_vertice] / self.total_mass[0] * self.r[
+            #     idx_vertice
+            # ].cross(
+            #     self.total_force[0]
+            # )
+            self.total_torque[0] += self.r[idx_vertice].cross(self.forces[idx_vertice])
+        self.omega[0] += self.dt * self.inertia[0].inverse() @ self.total_torque[0]
+        # Based on current omega, update the Interia and the r
+        # This is the same to the forumula treating current q is [0, 0, 0, 1] (xyzw)
+        self.rot_q_inc[0][3] = 1
+        self.rot_q_inc[0][:3] = self.dt / 2 * self.omega[0]
+        self.rot_q_inc[0] = self.rot_q_inc[0].normalized()
+        # Equal to below code to get the quaternion using axis angle
+        # radian = (self.dt * self.omega[0]).norm()
+        # direction = (self.dt * self.omega[0]).normalized()
+        # self.rot_q_inc[0][3] = ti.cos(radian / 2)
+        # self.rot_q_inc[0][0] = direction[0] * ti.sin(radian / 2)
+        # self.rot_q_inc[0][1] = direction[1] * ti.sin(radian / 2)
+        # self.rot_q_inc[0][2] = direction[2] * ti.sin(radian / 2)
+        self.rot_inc[0] = SetToRotate(self.rot_q_inc[0])
+
+        for i in range(self.n_vertices):
+            self.r[i] = self.rot_inc[0] @ self.r[i]
+        self.inertia[0] = (
+            self.rot_inc[0] @ self.inertia[0] @ self.rot_inc[0].transpose()
+        )
+
+    @ti.func
+    def process_collision(self):
+        pass
 
 
 def demo1():
@@ -88,6 +159,38 @@ def demo1():
         drag_damping=1,
     )
 
+    # Construct the visualizer
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    coordinate = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=1, origin=[0, 0, 0]
+    )
+    vis.add_geometry(coordinate)
+    # Define ground plane vertices
+    ground_vertices = np.array([[10, 10, 0], [10, -10, 0], [-10, -10, 0], [-10, 10, 0]])
+
+    # Define ground plane triangular faces
+    ground_triangles = np.array([[0, 2, 1], [0, 3, 2]])
+
+    # Create Open3D mesh object
+    ground_mesh = o3d.geometry.TriangleMesh()
+    ground_mesh.vertices = o3d.utility.Vector3dVector(ground_vertices)
+    ground_mesh.triangles = o3d.utility.Vector3iVector(ground_triangles)
+    ground_mesh.paint_uniform_color([1, 211 / 255, 139 / 255])
+    vis.add_geometry(ground_mesh)
+
+    for i in range(3000):
+        points = mySystem.step()
+        # print(spring_forces.min(), spring_forces.max(), spring_forces.mean(), np.median(spring_forces))
+        pcd.points = o3d.utility.Vector3dVector(points)
+        vis.update_geometry(pcd)
+
+        vis.poll_events()
+        vis.update_renderer()
+        # import pdb
+        # pdb.set_trace()
+    vis.destroy_window()
     pass
 
 
