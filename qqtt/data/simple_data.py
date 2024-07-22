fix the singleK gradient too small problem

qqtt/data/simple_data.py
@@ -14,15 +14,15 @@ class SimpleData:
         self.data = torch.tensor(self.data, dtype=torch.float32, device=cfg.device)
         self.frame_len = self.data.shape[0]
         self.point_num = self.data.shape[1]
-        if visualize:
-            # Visualize the GT frames
-            self.visualize_data()
+        # Visualize/save the GT frames
+        self.visualize_data(visualize=visualize)
 
-    def visualize_data(self):
-        visualize_pc(
-            self.data,
-            visualize=True,
-        )
+    def visualize_data(self, visualize=False):
+        if visualize:
+            visualize_pc(
+                self.data,
+                visualize=True,
+            )
         visualize_pc(
             self.data,
             visualize=False,

qqtt/engine/trainer.py
@@ -14,6 +14,7 @@ class InvPhyTrainer:
         cfg.data_path = data_path
         cfg.base_dir = base_dir
         cfg.device = device
+        cfg.run_name = base_dir.split("/")[-1]
         # Load the data
         self.dataset = SimpleData(visualize=False)
         # Initialize the vertices, springs, rest lengths and masses
@@ -41,10 +42,15 @@ class InvPhyTrainer:
         wandb.init(
             # set the wandb project where this run will be logged
             project="InvPhys",
-            name="SingleK/test1",
+            name=cfg.run_name,
             config=cfg.to_dict(),
-            # track hyperparameters and run metadata
         )
+        # wandb.init(
+        #     # set the wandb project where this run will be logged
+        #     project="Debug",
+        #     name="SingleK/test6",
+        #     config=cfg.to_dict(),
+        # )
         if not os.path.exists(f"{cfg.base_dir}/train"):
             # Create directory if it doesn't exist
             os.makedirs(f"{cfg.base_dir}/train")
@@ -93,14 +99,29 @@ class InvPhyTrainer:
             total_loss = 0.0
             for j in range(1, self.dataset.frame_len):
                 x, _, _, _ = self.simulator.step()
-                self.simulator.detach()
                 loss = self.compute_points_loss(self.dataset.data[j], x)
                 self.optimizer.zero_grad()
                 loss.backward()
+                grad = self.simulator.spring_Y.grad
                 self.optimizer.step()
+                self.simulator.detach()
                 total_loss += loss.item()
+                wandb.log(
+                    {
+                        "springY_detailed": torch.exp(self.simulator.spring_Y).item(),
+                        "grad": grad.item(),
+                        "iter_step": i * (self.dataset.frame_len - 1) + j,
+                        "iteration": i,
+                    }
+                )
             total_loss /= self.dataset.frame_len - 1
-            wandb.log({"loss": total_loss, "iteration": i})
+            wandb.log(
+                {
+                    "springY": torch.exp(self.simulator.spring_Y).item(),
+                    "loss": total_loss,
+                    "iteration": i,
+                }
+            )
 
             logger.info(f"[Train]: Iteration: {i}, Loss: {total_loss}")
 
@@ -113,8 +134,8 @@ class InvPhyTrainer:
                             video_path,
                             format="mp4",
                             fps=cfg.FPS,
-                        )
-                    }
+                        ),
+                    },
                 )
                 logger.info(f"[Visualize]: Visualize the simulation at iteration {i}")
 

qqtt/model/diff_simulator/spring_mass.py
@@ -34,8 +34,13 @@ class SpringMassSystem(nn.Module):
 
         self.dt = dt
         self.num_substeps = num_substeps
+        # Use the log value to make it easier to learn
         self.spring_Y = nn.Parameter(
-            torch.tensor(spring_Y, dtype=torch.float32, device=self.device),
+            torch.tensor(
+                torch.log(torch.tensor(spring_Y)),
+                dtype=torch.float32,
+                device=self.device,
+            ),
             requires_grad=True,
         )
         self.dashpot_damping = dashpot_damping
@@ -85,7 +90,7 @@ class SpringMassSystem(nn.Module):
         dis = x2 - x1
         d = dis / torch.norm(dis, dim=1)[:, None]
         self.spring_forces = (
-            self.spring_Y
+            torch.exp(self.spring_Y)
             * (torch.norm(dis, dim=1) / self.rest_lengths - 1)[:, None]
             * d
         )