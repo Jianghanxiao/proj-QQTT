fd091e72d67467d470e2da7d46baa1bb003b745b
Jianghanxiao <shawn.x.jiang@gmail.com>
2024-10-24 14:48:16 -0400

    finish the code to recording; todo: need the extra post-processing to get better alignment and get the pc

--- qqtt/env/camera/camera_system.py
+++ qqtt/env/camera/camera_system.py
@@ -2,21 +2,29 @@ from realsense import MultiRealsense, SingleRealsense
 from multiprocessing.managers import SharedMemoryManager
 import numpy as np
 import time
+from pynput import keyboard
+import cv2
+from datetime import datetime
+import json
+import os
 
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(suppress=True)
 
 
+def exist_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
 class CameraSystem:
-    def __init__(self, WH=[640, 480], fps=30, num_cameras=4):
+    def __init__(self, WH=[640, 480], fps=30, num_cam=4):
         self.WH = WH
         self.fps = fps
 
         self.serial_numbers = SingleRealsense.get_connected_devices_serial()
-        self.n_cameras = len(self.serial_numbers)
-        assert (
-            self.n_cameras == num_cameras
-        ), f"Only {self.n_cameras} cameras are connected."
+        self.num_cam = len(self.serial_numbers)
+        assert self.num_cam == num_cam, f"Only {self.num_cam} cameras are connected."
 
         self.shm_manager = SharedMemoryManager()
         self.shm_manager.start()
@@ -37,6 +45,11 @@ class CameraSystem:
 
         self.realsense.start()
         time.sleep(3)
+        self.recording = False
+        self.end = False
+        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.listener.start()
+        print("Camera system is ready.")
 
     def get_observation(self):
         # Used to get the latest observations from all cameras
@@ -69,7 +82,75 @@ class CameraSystem:
 
         return data
 
+    def on_press(self, key):
+        try:
+            if key == keyboard.Key.space:
+                if self.recording == False:
+                    self.recording = True
+                    print("Start recording")
+                else:
+                    self.recording = False
+                    self.end = True
+        except AttributeError:
+            pass
+
+    def record(self, output_path):
+        exist_dir(output_path)
+        exist_dir(f"{output_path}/color")
+        exist_dir(f"{output_path}/depth")
+
+        metadata = {}
+        intrinsics = self.realsense.get_intrinsics()
+        metadata["intrinsics"] = intrinsics
+        metadata["serial_numbers"] = self.serial_numbers
+        metadata["fps"] = self.fps
+        metadata["WH"] = self.WH
+        metadata["recording"] = {}
+        for i in range(self.num_cam):
+            metadata["recording"][i] = {}
+            exist_dir(f"{output_path}/color/{i}")
+            exist_dir(f"{output_path}/depth/{i}")
+
+        # Set the max time for recording
+        last_step_idxs = [-1] * self.num_cam
+        while not self.end:
+            if self.recording:
+                last_realsense_data = self.realsense.get()
+                timestamps = [
+                    last_realsense_data[i]["timestamp"].item()
+                    for i in range(self.num_cam)
+                ]
+                step_idxs = [
+                    last_realsense_data[i]["step_idx"].item()
+                    for i in range(self.num_cam)
+                ]
+
+                if not all(
+                    [step_idxs[i] == last_step_idxs[i] for i in range(self.num_cam)]
+                ):
+                    for i in range(self.num_cam):
+                        if last_step_idxs[i] != step_idxs[i]:
+                            # Record the the step for this camera
+                            time_stamp = timestamps[i]
+                            step_idx = step_idxs[i]
+                            color = last_realsense_data[i]["color"]
+                            depth = last_realsense_data[i]["depth"]
+
+                            metadata["recording"][i][step_idx] = time_stamp
+                            cv2.imwrite(
+                                f"{output_path}/color/{i}/{step_idx}.png", color
+                            )
+                            np.save(f"{output_path}/depth/{i}/{step_idx}.npy", depth)
+
+        print("End recording")
+        self.listener.stop()
+        with open(f"{output_path}/metadata.json", "w") as f:
+            json.dump(metadata, f)
+
 
 if __name__ == "__main__":
     camera_system = CameraSystem()
-    camera_system._get_sync_frame()
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    camera_system.record(
+        output_path=f"/home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/data_collect/{current_time}"
+    )
