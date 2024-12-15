a167b4d88dc06096b8e268834d5e506d6732f4ea
Jianghanxiao <shawn.x.jiang@gmail.com>
2024-12-15 13:30:44 -0500

    add the yaml config system

--- qqtt/utils/config.py
+++ qqtt/utils/config.py
@@ -1,47 +1,15 @@
 from .misc import singleton
+import yaml
 
 
 @singleton
-class SimpleConfig:
+class Config:
     def __init__(self):
-        self.FPS = 10
-        self.dt = 5e-5
-        self.num_substeps = 100
-        self.dashpot_damping = 100
-        self.drag_damping = 3
-        self.base_lr = 1e-3
-        self.iterations = 500
-        self.vis_interval = self.iterations / 10
-        self.init_spring_Y = 3e3
-        self.init_collide_elas = 0.5
-        self.init_collide_fric = 0.3
-        self.collide_object_elas = 0.7
-        self.collide_object_fric = 0.3
-
-        self.radius = 0.1
-        self.max_neighbours = 20
-
-        # Parameters on whether update the collision parameters
-        self.collision_learn = True
-
-        # Parameters on whether the gt point cloud is ordered across time
-        self.match = True
-
-    def to_dict(self):
-        # Convert the class to dictionary
-        return {
-            attr: getattr(self, attr)
-            for attr in dir(self)
-            if not callable(getattr(self, attr)) and not attr.startswith("__")
-        }
-
-
-@singleton
-class RealConfig:
-    def __init__(self):
+        self.data_type = None
         self.FPS = 30
         self.dt = 5e-5
         self.num_substeps = round(1.0 / self.FPS / self.dt)
+
         self.dashpot_damping = 100
         self.drag_damping = 3
         self.base_lr = 1e-3
@@ -52,10 +20,6 @@ class RealConfig:
         self.init_collide_fric = 0.3
         self.collide_object_elas = 0.7
         self.collide_object_fric = 0.3
-
-        self.chamfer_weight = 1.0
-        self.track_weight = 1.0
-        self.acc_weight = 0.01
 
         self.radius = 0.01
         self.max_neighbours = 20
@@ -69,6 +33,11 @@ class RealConfig:
         # DEBUG mode: set use_graph to False
         self.use_graph = True
 
+        # Attribute for the real
+        self.chamfer_weight = 1.0
+        self.track_weight = 1.0
+        self.acc_weight = 0.01
+
     def to_dict(self):
         # Convert the class to dictionary
         return {
@@ -77,6 +46,20 @@ class RealConfig:
             if not callable(getattr(self, attr)) and not attr.startswith("__")
         }
 
+    def update_from_dict(self, config_dict):
+        for key, value in config_dict.items():
+            if hasattr(self, key):
+                current_value = getattr(self, key)
+                if isinstance(current_value, int):
+                    value = int(value)
+                elif isinstance(current_value, float):
+                    value = float(value)
+                setattr(self, key, value)
 
-# cfg = SimpleConfig()
-cfg = RealConfig()
+    def load_from_yaml(self, file_path):
+        with open(file_path, "r") as file:
+            config_dict = yaml.safe_load(file)
+        self.update_from_dict(config_dict)
+
+
+cfg = Config()
--- train_warp.py
+++ train_warp.py
@@ -22,9 +22,12 @@ set_all_seeds(seed)
 
 
 def demo_real():
+    cfg.load_from_yaml("configs/real.yaml")
+    print(f"[DATA TYPE]: {cfg.data_type}")
+
     # current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     # base_dir = f"experiments/{current_time}"
-    base_dir = f"experiments/warp_rope_acc"
+    base_dir = f"experiments/debug_warp_rope_acc"
     cfg.spring_Y_min = 0
     # cfg.init_spring_Y = 1e3
     logger.set_log_file(path=base_dir, name="inv_phy_log")
@@ -39,5 +42,25 @@ def demo_real():
     # trainer.test("/home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/experiments/debug_warp/train/iter_0.pth")
 
 
+def demo_multiple_k():
+    cfg.load_from_yaml("configs/synthetic.yaml")
+    print(f"[DATA TYPE]: {cfg.data_type}")
+
+    # current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # base_dir = f"experiments/{current_time}"
+    base_dir = f"experiments/table_2k"
+    cfg.num_substeps = 1000
+    cfg.init_spring_Y = 3e4
+    logger.set_log_file(path=base_dir, name="inv_phy_log")
+    trainer = RealInvPhyTrainerWarp(
+        data_path=f"/home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/taichi_simulator_test/data_prepare/multiple_k_data_prepare/table_2k.npy",
+        base_dir=base_dir,
+    )
+    # trainer.visualize_sim(save_only=False)
+    trainer.train()
+    # trainer.test("/home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/experiments/TwoK/train/iter_199.pth")
+
+
 if __name__ == "__main__":
-    demo_real()
+    # demo_real()
+    demo_multiple_k()
