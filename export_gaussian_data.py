8e183e2c87e41b894eeeaaa9c8c7bff71d46f060
Jianghanxiao <shawn.x.jiang@gmail.com>
2025-02-17 02:12:39 -0500

    fix export gaussian; change the train script

--- export_gaussian_data.py
+++ export_gaussian_data.py
@@ -23,6 +23,7 @@ with open("data_config.csv", newline="", encoding="utf-8") as csvfile:
         case_name = row[0]
         category = row[1]
         shape_prior = row[2]
+        print(f"Processing {case_name}!!!!!!!!!!!!!!!")
 
         # Create the directory for the case
         existDir(f"{output_path}/{case_name}")
@@ -43,19 +44,16 @@ with open("data_config.csv", newline="", encoding="utf-8") as csvfile:
                     obj_idx = int(key)
             mask_path = f"{base_path}/{case_name}/mask/{i}/{obj_idx}/0.png"
             os.system(f"cp {mask_path} {output_path}/{case_name}/mask_{i}.png")
-            # Prepare the high-resolution image. If it's already generated, then copy it
-            if i == 0 and shape_prior.lower() == "true":
-                os.system(
-                    f"cp {base_path}/{case_name}/shape/high_resolution.png {output_path}/{case_name}/{i}_high.png"
-                )
-            else:
-                os.system(
-                    f"python ./data_process/image_upscale.py --img_path {base_path}/{case_name}/color/{i}/0.png --output_path {output_path}/{case_name}/{i}_high.png --category {category}"
-                )
+            # Prepare the high-resolution image
+            os.system(
+                f"python ./data_process/image_upscale.py --img_path {base_path}/{case_name}/color/{i}/0.png --output_path {output_path}/{case_name}/{i}_high.png --category {category}"
+            )
             # Prepare the segmentation mask of the high-resolution image
             os.system(
                 f"python ./data_process/segment_util_image.py --img_path {output_path}/{case_name}/{i}_high.png --TEXT_PROMPT {category} --output_path {output_path}/{case_name}/mask_{i}_high.png"
             )
+            break
+        continue
 
         # Prepare the intrinsic and extrinsic parameters
         with open(f"{base_path}/{case_name}/calibrate.pkl", "rb") as f:
